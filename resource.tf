7c4be95cb6267b148b2aba7d8ce4d3f4d8494bf5
jksoam <jksoam007@gmail.com>
2025-01-24 17:34:59 +0530

    additionvpcworkingfor 3 EC2

--- resource.tf
+++ resource.tf
@@ -1,14 +1,16 @@
 resource "aws_instance" "demo-server" {
-  ami             = "ami-04b4f1a9cf54c11d0"  # Replace with your desired AMI ID
-  instance_type   = "t2.micro"
-  key_name        = "vk1234"  # Replace with your key name
+  for_each = toset(["Jenkin-Master", "Jankin-slave", "Ansible"]) # Use a set of values
+
+  ami           = "ami-04b4f1a9cf54c11d0" # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  key_name      = "vk1234" # Replace with your key name
 
   # Correct way to assign security group to the EC2 instance
   vpc_security_group_ids = [aws_security_group.demo-sg.id]
 
-  subnet_id = aws_subnet.public_subnet.id  # Ensure you have the correct subnet
+  subnet_id = aws_subnet.public_subnet.id # Ensure you have the correct subnet
 
   tags = {
-    Name = "Demo-Server"
+    Name = each.key # Each key will be one of the set values: Jenkin-Master, Jankin-slave, Ansible
   }
 }
