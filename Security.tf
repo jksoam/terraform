cf9a9445c6632071c61cb22aea92f96d5df1eeda
jksoam <jksoam007@gmail.com>
2025-01-24 15:04:59 +0530

    addition

---
+++ Security.tf
@@ -0,0 +1,39 @@
+# Create a Security Group
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow inbound HTTP, HTTPS, and SSH traffic"
+  vpc_id      = "vpc-xxxxxx" # Replace with your VPC ID
+
+  # Inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your specific IP or CIDR block for SSH
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS from anywhere
+  }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
---
+++ resource.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "demo-server" {
+  ami             = "ami-04b4f1a9cf54c11d0"
+  instance_type   = "t2.micro"
+  key_name        = "vk1234"
+  security_groups = ["demo-sg"]
+}
